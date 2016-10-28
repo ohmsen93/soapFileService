29b592696e09068ffe71dbc5ce90e37801990c1c
Mads Bjørk Ohmsen <mads696d@edu.easj.dk>
2016-10-28 11:04:52 +0200

    Add project files.

---
+++ soapFileService/Model/File.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Runtime.Serialization;
+
+namespace soapFileService
+{
+    [DataContract]
+    public class File
+    {
+        private string _fileName;
+        private IPAddress _ip;
+        private int _port;
+        
+        [DataMember]
+        public string FileName
+        {
+            get { return _fileName; }
+            set { _fileName = value; }
+        }
+
+        [DataMember]
+        public IPAddress Ip
+        {
+            get { return _ip; }
+            set { _ip = value; }
+        }
+
+        [DataMember]
+        public int Port
+        {
+            get { return _port; }
+            set { _port = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"FileName: {FileName}, Ip: {Ip}, Port: {Port}";
+        }
+    }
+}
---
+++ soapFileService/Model/FileListSingleton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+
+namespace soapFileService
+{
+    [DataContract]
+    public class FileListSingleton
+    {
+        [DataMember]
+        public static List<string> FileList { get; set; } 
+    }
+}
